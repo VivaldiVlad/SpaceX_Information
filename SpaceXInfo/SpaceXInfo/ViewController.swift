55c88c0dc710008551f868d91537d005e06f61bb
Vladislav <v.i.poberezhnik@yandex.ru>
2022-03-20 19:52:41 +0500

    Создание проекта

---
+++ SpaceXInfo/SpaceXInfo/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  SpaceXInfo
+//
+//  Created by Vladislav on 20.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
