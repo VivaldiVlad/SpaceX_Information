435a8a9e5abadd58967099f965d2e2683aa4ca63
Vladislav <v.i.poberezhnik@yandex.ru>
2022-03-25 19:29:56 +0500

    Start project

---
+++ SpaceX/SpaceX/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SpaceX
+//
+//  Created by Vladislav on 25.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
