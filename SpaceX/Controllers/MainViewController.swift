549dafb120c9ce9f6e88ff61c07f6fe30ce60f10
KAKPONOTAM <sabitovdanill@icloud.com>
2022-03-26 10:31:47 +0300

    added_collection_view_and_custom_cell

--- SpaceX/Controllers/MainViewController.swift
+++ SpaceX/Controllers/MainViewController.swift
@@ -2,12 +2,13 @@ import UIKit
 
 class MainViewController: UIViewController {
     //MARK: - properties
-    private let contentScrollView: UIScrollView = {
-        let scrollView = UIScrollView()
-        scrollView.translatesAutoresizingMaskIntoConstraints = false
-        scrollView.backgroundColor = .red
-        scrollView.isPagingEnabled = true
-        return scrollView
+    private let collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.minimumLineSpacing = 5
+        layout.scrollDirection = .horizontal
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        return collectionView
     }()
     
     private let pageControl: UIPageControl = {
@@ -24,12 +25,13 @@ class MainViewController: UIViewController {
         addSubview()
         constraintsSetup()
         navigationController?.navigationBar.isHidden = true
+        collectionViewSetup()
     }
     
     //MARK: - methods
     private func addSubview() {
         view.addSubview(pageControl)
-        view.addSubview(contentScrollView)
+        view.addSubview(collectionView)
     }
     
     private func constraintsSetup() {
@@ -41,10 +43,35 @@ class MainViewController: UIViewController {
         ])
         
         NSLayoutConstraint.activate([
-            contentScrollView.topAnchor.constraint(equalTo: view.topAnchor),
-            contentScrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            contentScrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            contentScrollView.bottomAnchor.constraint(equalTo: pageControl.topAnchor)
+            collectionView.topAnchor.constraint(equalTo: view.topAnchor),
+            collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: pageControl.topAnchor)
         ])
+    }
+    
+    private func collectionViewSetup() {
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        
+        collectionView.register(CustomCollectionViewCell.self, forCellWithReuseIdentifier: CustomCollectionViewCell.identifier)
+    }
+}
+
+extension MainViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return pageControl.numberOfPages
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CustomCollectionViewCell.identifier, for: indexPath) as? CustomCollectionViewCell else { return UICollectionViewCell() }
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width = collectionView.frame.width
+        let height = collectionView.frame.height
+        
+        return CGSize(width: width, height: height)
     }
 }
---
+++ SpaceX/CustomElements/CustomCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  CustomCollectionViewCell.swift
+//  SpaceX
+//
+//  Created by Сабитов Данил on 26.03.2022.
+//
+
+import UIKit
+
+class CustomCollectionViewCell: UICollectionViewCell {
+    
+}
